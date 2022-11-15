8e13733eb25b1b5b586a214e3f94a53e7248f1a5
Artemiy-Z <artemii.zubarev@gmail.com>
2022-11-16 00:14:06 +0500

    initial commit

---
+++ app/src/main/java/com/example/Module1/EnterYourHouse/EnterActivity.java
@@ -0,0 +1,16 @@
+package com.example.Module1.EnterYourHouse;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.exmod1.R;
+
+public class EnterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter);
+    }
+}
