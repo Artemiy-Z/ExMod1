e62da58d320d57080fc2682045d6098b98e2f20b
a <aaa>
2022-11-16 11:52:59 +0500

    some upgrades

---
+++ app/src/main/java/com/example/Module1/AddRoom/AddRoomActivity.java
@@ -0,0 +1,16 @@
+package com.example.Module1.AddRoom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.Module1.R;
+
+public class AddRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_room);
+    }
+}
